20f49542c359d05b2f038eb2ce15d1f622e25ee5
Maksim <akiam@mail.ru>
2015-10-22 13:07:57 +0300

    Looks incredible!

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
     var open = 0;
-
-    $(".message-button-holder").on("click", function () {
+    $("#incredible-button").on("click", function () {
         if (open == 1) {
-            $(this).removeClass("message-button-holder-active");
+            $(this).removeClass("message-center-button-holder-active").addClass("message-center-button-holder");
+            function unFillWindow() {
+                return $("#incredible-button").removeClass("message-center-button-holder").addClass("message-button-holder");
+            }
+            setTimeout(unFillWindow, 900);
             open--;
         } else {
             open++;
-            $(this).addClass("message-button-holder-active")
+            $("#incredible-button").removeClass("message-button-holder").addClass("message-center-button-holder");
+            function fillWindow() {
+                return $("#incredible-button").removeClass("message-center-button-holder").addClass("message-center-button-holder-active");
+            }
+
+            setTimeout(fillWindow, 900);
         }
     });
-
 });
