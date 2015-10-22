911c8930b1888ad8892c77ab7a223b8de1986b61
Maksim <akiam@mail.ru>
2015-10-22 14:19:50 +0300

    Added form and some minimize of code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
+/*
+This button was created by Maksim Loginov
+Contact me: akiam@mail.ru
+&copy 2015
+ */
 $(document).ready(function () {
     var open = 0;
     $("#incredible-button").on("click", function () {
+        var self = $(this);
         if (open == 1) {
-            $(this).removeClass("message-center-button-holder-active").addClass("message-center-button-holder");
+            self.removeClass("message-center-button-holder-active").addClass("message-center-button-holder");
+            $("#message-form-holder").fadeOut(350);
             function unFillWindow() {
-                return $("#incredible-button").removeClass("message-center-button-holder").addClass("message-button-holder");
+                self.removeClass("message-center-button-holder").addClass("message-button-holder");
             }
+
             setTimeout(unFillWindow, 900);
             open--;
         } else {
             open++;
-            $("#incredible-button").removeClass("message-button-holder").addClass("message-center-button-holder");
+            self.removeClass("message-button-holder").addClass("message-center-button-holder");
             function fillWindow() {
-                return $("#incredible-button").removeClass("message-center-button-holder").addClass("message-center-button-holder-active");
+                self.removeClass("message-center-button-holder").addClass("message-center-button-holder-active");
+                $("#message-form-holder").fadeIn(1000);
             }
 
             setTimeout(fillWindow, 900);
